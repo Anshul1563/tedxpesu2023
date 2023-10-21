2e719a32c6d39459ebe18440fb20274aceb5c0f8
sujay1844 <sujay1844@gmail.com>
2023-10-21 17:50:36 +0530

    Added venue

---
+++ app/Venue.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image"
+import { useEffect, useRef } from "react"
+import VenueImg from '@/public/Images/mrd.png'
+
+export default function Venue() {
+    const imageRef = useRef<HTMLImageElement | null>(null)
+
+    const handleScroll = () => {
+        const scrollValue = window.scrollY
+        imageRef.current!.style.transform = `translateY(+${scrollValue * 0.1}px)`
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
+    return (
+        <section>
+            <div className="flex flex-col relative">
+                <div className="w-full mix-blend-overlay flex relative">
+                    <Image ref={imageRef} className="w-full h-full object-cover" src={VenueImg} alt="MRD Auditorium" width={1000} height={1000} />
+                    <div className="absolute h-full w-full bg-gradient-to-b from-transparent to-black via-black via-[90%]"></div>
+                </div>
+                <div className="absolute bottom-[10%] text-white pl-24 flex flex-col gap-2">
+                    <h2 className="text-2xl mb-4">Our Location</h2>
+                    <h1 className="text-5xl">GJB Auditorium <br/> PES University</h1>
+                </div>
+            </div>
+            <div className="px-24 bg-black text-white whitespace-normal text-xl pr-[50%] text-justify">
+                <p>Venue description goes here</p>
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+            </div>
+        </section>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+'use client'
 import About from "./About";
+import Venue from "./Venue";
 
 export default function Home() {
 	return (
 		<div className="">
 			<About />
+			<Venue />
 		</div>
 	);
 }
