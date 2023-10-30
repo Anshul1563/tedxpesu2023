ce142cb3460ba554ad284bd1f68bdc0024244fc7
Anshul Dasyam <pesanshul@gmail.com>
2023-10-30 23:50:19 +0530

    venue changes

--- app/AltVenue.tsx
+++ app/AltVenue.tsx
@@ -2,7 +2,7 @@ function AltVenue() {
 	return (
 		<div className="h-screen bg-[url('/images/gjbc.jpeg')] bg-cover bg-no-repeat bg-center font-work relative text-white -mt-1">
 			<div className="absolute h-full w-full bg-black opacity-80"></div>
-			<div className="w-fit flex flex-col items-center px-4 phone:px-0 justify-center phone:h-auto h-full phone:block phone:absolute phone:bottom-40 phone:right-20">
+			<div className="w-full phone:w-fit flex flex-col items-center px-4 phone:px-0 justify-center phone:h-auto h-full phone:block phone:absolute phone:bottom-40 phone:right-20">
 				<p className="phone:text-4xl text-2xl text-start mb-4 font-bold opacity-90 phone:opacity-100">
 					VENUE
 				</p>
